74ed81e4b049ac93585d7564282468188af59264
AlexisJmz <alexis.jmzpr@gmail.com>
2022-11-12 10:14:25 -0600

    Agregado archivo de menu

---
+++ ProyectoU2.py
@@ -0,0 +1,25 @@
+print("¿Que desea hacer?")
+print("1. Suma de numeros")
+print("2. Multiplicacion de numeros")
+print("3. Division")
+print("4. Calcular Factorial")
+print("5. Tablas de multiplicar")
+print("6. Cuadrado y cubo de un numero")
+print("7. Promedio de una serie de numeros")
+print("8. Maximo y minimo")
+print("9. Salir")
+o = input("Escoja su opcion:")
+match o:
+# CODIGO DE SARA
+    case '1':
+    case '2':
+# CODIGO DE CYNTHIA
+    case '3':
+    case '4':
+# CODIGO DE XIMENA
+    case '5':
+    case '6':
+# CODIGO DE ALEXIS
+    case '7':
+    case '8':
+    case '9':
