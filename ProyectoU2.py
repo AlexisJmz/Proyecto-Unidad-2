4a5dea6a5b8f164640be3ba5f9d605758e413c41
AbigailRobles <1930580@utmatamoros.edu.mx>
2022-11-12 17:11:58 -0600

    SABR - Agregadas opciones de suma y multiplicacion

--- ProyectoU2.py
+++ ProyectoU2.py
@@ -12,7 +12,65 @@ o = input("Escoja su opcion:")
 match o:
 # CODIGO DE SARA
     case '1':
+        opcion = int(input("Cuantos numeros quieres sumar? (min 2, max 5):"))
+        if opcion == 2:
+            print("Ingrese sus 2 numeros")
+            num1 = int(input("Primer numero:"))
+            num2 = int(input("Segundo numero:"))
+            print("La suma de", num1,"+", num2, "es:", num1+num2)
+        elif opcion == 3:
+            print("Ingrese sus 3 numeros")
+            num1 = int(input("Primer numero:"))
+            num2 = int(input("Segundo numero:"))
+            num3 = int(input("Tercer numero:"))
+            print("La suma de", num1,"+", num2,"+", num3,"es:", num1+num2+num3)
+        elif opcion == 4:
+            print("Ingrese sus 4 numeros")
+            num1 = int(input("Primer numero:"))
+            num2 = int(input("Segundo numero:"))
+            num3 = int(input("Tercer numero:"))
+            num4 = int(input("Cuarto numero:"))
+            print("La suma de", num1,"+", num2,"+", num3,"+", num4,"es:", num1+num2+num3+num4)
+        elif opcion == 5:
+            print("Ingrese sus 5 numeros")
+            num1 = int(input("Primer numero:"))
+            num2 = int(input("Segundo numero:"))
+            num3 = int(input("Tercer numero:"))
+            num4 = int(input("Cuarto numero:"))
+            num5 = int(input("Quinto numero:"))
+            print("La suma de", num1,"+", num2,"+", num3,"+", num4,"+", num5,"es:", num1+num2+num3+num4+num5)
+        else:
+            print("Seleccione otra opcion")
     case '2':
+        opcion = int(input("Cuantos numeros quieres multiplicar? (min 2, max 5):"))
+        if opcion == 2:
+            print("Ingrese sus 2 numeros")
+            num1 = int(input("Primer numero:"))
+            num2 = int(input("Segundo numero:"))
+            print("La multiplicacion de", num1,"x", num2, "es:", num1*num2)
+        elif opcion == 3:
+            print("Ingrese sus 3 numeros")
+            num1 = int(input("Primer numero:"))
+            num2 = int(input("Segundo numero:"))
+            num3 = int(input("Tercer numero:"))
+            print("La multiplicacion de", num1,"x", num2,"x", num3,"es:", num1*num2*num3)
+        elif opcion == 4:
+            print("Ingrese sus 4 numeros")
+            num1 = int(input("Primer numero:"))
+            num2 = int(input("Segundo numero:"))
+            num3 = int(input("Tercer numero:"))
+            num4 = int(input("Cuarto numero:"))
+            print("La multiplicacion de", num1,"x", num2,"x", num3,"x", num4,"es:", num1*num2*num3*num4)
+        elif opcion == 5:
+            print("Ingrese sus 5 numeros")
+            num1 = int(input("Primer numero:"))
+            num2 = int(input("Segundo numero:"))
+            num3 = int(input("Tercer numero:"))
+            num4 = int(input("Cuarto numero:"))
+            num5 = int(input("Quinto numero:"))
+            print("La multiplicacion de", num1,"x", num2,"x", num3,"x", num4,"x", num5,"es:", num1*num2*num3*num4*num5)
+        else:
+            print("Seleccione otra opcion")
 # CODIGO DE CYNTHIA
     case '3':
     case '4':
